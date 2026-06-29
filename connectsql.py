34fcca751700c6b92d2a7fd1fe97dea10fe8d1ac
Nickolas <nickolas.merola@gmail.com>
2026-06-28 22:33:17 -0300

    explicando minha parte

--- connectsql.py
+++ connectsql.py
@@ -43,9 +43,12 @@ def fechar_execusao(conexao, cursor):
             pass
 
 def enviar_email_relatorio(id_operador, resposta, assunto):
+    
+    # Uso do getenv para puxar a variavel do arquivo env onde existe uma senha para essa função funcionar!
     email_origem = "viniciusgsales789@gmail.com" 
     senha_app = os.getenv("GMAIL")
     
+    # Trava de segurança para o conexão funcionar corretamente
     conexao = obter_conexao()
     if not conexao:
         print(Fore.RED + Style.BRIGHT + "\n[✖] ERRO: Sistema offline. Não foi possível acessar os dados para enviar o e-mail." + Fore.RESET)
@@ -54,6 +57,8 @@ def enviar_email_relatorio(id_operador, resposta, assunto):
     cursor = conexao.cursor()
     
     try:
+        # Para buscar o e-mail do usuário logado, utilizo o %s
+        # isso impede tentativas de injeção de SQL no banco de dados.
         cursor.execute("SELECT gmail FROM usuarios WHERE id_usuario = %s AND ativo = 1", (id_operador,))
         result = cursor.fetchone()
         
@@ -62,12 +67,15 @@ def enviar_email_relatorio(id_operador, resposta, assunto):
             return False
             
         destinatario = result[0]
+        
+    # Ofinally garante o fechamente do banco de dados caso QUALQUER coisa der errado porque é tratado como uma função de prioridade 
     finally:
         fechar_execusao(
             conexao if "conexao" in locals() else None, 
             cursor if "cursor" in locals() else None
         )
     
+    # Para montar a estrutura do e-mail, usei a classe EmailMessage para o codigo ficar mais seguro e limpo
     msg = EmailMessage()
     msg['Subject'] = assunto
     msg['From'] = email_origem
@@ -77,6 +85,8 @@ def enviar_email_relatorio(id_operador, resposta, assunto):
     try:
         print(Fore.YELLOW + "\n[!] Processando o envio da mensagem, aguarde..." + Fore.RESET)
         
+        # O bloco 'with' abre a conexão SMTP(serve para que o codigo consiga conversar com o servidor a partir de uma porta) e já fecha sozinho quando termina. 
+        # O comando starttls cria um túnel criptografado para o envio seguro(ferramenta obrigatoria quando falamos de GMAIL)
         with smtplib.SMTP('smtp.gmail.com', 587) as servidor:
             servidor.starttls()
             servidor.login(email_origem, senha_app)
@@ -84,7 +94,8 @@ def enviar_email_relatorio(id_operador, resposta, assunto):
             
         print(Fore.GREEN + f"\n[✔] SUCESSO: E-mail enviado para {destinatario}." + Fore.RESET)
         return True
-
+    
+    #separei os erros, caso for na rede ou SMPT ele avisa, mas caso não funcione acionei o protocolo padrão de erro que usamos em todo o codigo
     except smtplib.SMTPException as erro_smtp:
         print(Fore.RED + Style.BRIGHT + f"\n[✖] ERRO: Falha na conexão de rede/e-mail: {erro_smtp}" + Fore.RESET)
         return False
